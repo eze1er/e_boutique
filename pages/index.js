2ccccd100bfd8574763a0412ae5504d5dd7919a4
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-07-19 20:10:56 -0400

    hero banner done

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,39 @@
 import React from "react";
+
+import { client } from '../lib/client';
+
 import { Product, FooterBanner, HeroBanner } from "../components";
-const Home = () => {
+const Home = ({ products, bannerData}) => {
   return (
     <div>
-      <HeroBanner />
-
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div className="products-container">
-        {["product 01", "product 02", "product 03"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner />
     </div>
   );
 };
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {
+      products, bannerData
+    }
+  }
+  
+}
 
 export default Home;
