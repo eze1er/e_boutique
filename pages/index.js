c7454dce9e7babb05828e583be14b1d339af9f98
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-07-19 21:06:42 -0400

    children props in layout add

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const Home = ({ products, bannerData}) => {
         {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
-      <FooterBanner />
+      <FooterBanner FooterBanner={bannerData && bannerData[0]} />
     </div>
   );
 };
